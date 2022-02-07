43dd65b86d20deb3f3cafd3d27866b27e4865c83
avdao <76061468+avdao@users.noreply.github.com>
2022-02-07 14:28:58 +0100

    Add files via upload

---
+++ node_project/utils/cloudinary.js
@@ -0,0 +1,11 @@
+
+
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+    cloud_name: 'dpegkr84d',
+    api_key: '115223796366192',
+    api_secret: 'b6LeFOhogQtZHud3waB0xrggiGw',
+});
+
+module.exports = cloudinary;
