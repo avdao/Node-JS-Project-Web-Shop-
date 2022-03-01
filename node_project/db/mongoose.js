4653003245283542c77e9b8537de0b73dfa7797b
avdao <76061468+avdao@users.noreply.github.com>
2022-03-01 15:10:33 +0100

    Update mongoose.js

--- node_project/db/mongoose.js
+++ node_project/db/mongoose.js
@@ -2,5 +2,5 @@
 const mongoose = require("mongoose");
 
 
-mongoose.connect("mongodb+srv://avdao:bsphkh123@cluster0.tjxba.mongodb.net/mern_blog?retryWrites=true&w=majority",
+mongoose.connect("mongodb+srv://avdao:admin123@cluster0.tjxba.mongodb.net/mern_blog?retryWrites=true&w=majority",
     { useUnifiedTopology: true, useNewUrlParser: true})
